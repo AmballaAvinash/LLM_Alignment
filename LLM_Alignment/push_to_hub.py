1f5d648b074b19c041a15fea147ec0c4ed095e6f
Avinash Amballa <amballaavinash@gmail.com>
2024-05-14 00:39:39 -0400

    test

--- LLM_Alignment/push_to_hub.py
+++ LLM_Alignment/push_to_hub.py
@@ -14,6 +14,8 @@ import argparse
 
 import transformers
 
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, BitsAndBytesConfig
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from huggingface_hub import notebook_login
 
@@ -23,16 +25,31 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false" # to avoid warning "Tokenizer dea
 os.environ["HF_TOKEN"] = "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" # my huggingface key to access llama models
 
 
-# Login to HF Hub
-notebook_login()
-
-
 model_id = "./saved-models/DPO_LLAMA-7B/merged_model"
 device = "cuda"
-dtype = torch.bfloat16
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+print("############### BNB config done")
+
+model = AutoModelForCausalLM.from_pretrained(model_id, 
+                                             quantization_config=bnb_config, 
+                                             device_map={"": 0},
+                                            use_cache=False)
+
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map=device)
+
+
+
+
+# Login to HF Hub
+notebook_login()
 
 
 model.push_to_hub('LLAMA-7B-8000')
