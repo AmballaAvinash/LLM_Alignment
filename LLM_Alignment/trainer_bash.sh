868d325211bb8905b583a7659ead963228ff6b5d
Avinash Amballa <amballaavinash@gmail.com>
2024-05-15 00:37:01 -0400

    test

--- LLM_Alignment/trainer_bash.sh
+++ LLM_Alignment/trainer_bash.sh
@@ -45,7 +45,7 @@ python LLM_Alignment/gpu_check.py
 
 python LLM_Alignment/DPO_trainer_LLAMA.py \
       --model_name_or_path dsaluru/Instruction-Tuned-LLaMa-7B-Alpaca-no-quant\
-      --train_size 0.5\
+      --train_size 0.25\
       --per_device_train_batch_size 4 \
       --per_device_eval_batch_size 4 \
       --gradient_accumulation_steps 1 \
@@ -55,13 +55,13 @@ python LLM_Alignment/DPO_trainer_LLAMA.py \
       --max_seq_length 1024 \
       --num_train_epochs 2 \
       --evaluation_strategy steps \
-      --eval_steps 200 \
+      --eval_steps 100 \
       --logging_strategy steps \
-      --log_steps 200 \
+      --log_steps 100 \
       --logging_first_step \
       --save_strategy epoch \
       --save_steps 1 \
       --lora_rank 8 \
       --lora_alpha 32 \
       --lora_dropout 0.1 \
-      --output_dir ./saved-models/DPO_LLAMA-7B_0.5
+      --output_dir ./saved-models/DPO_LLAMA-7B_0.25
